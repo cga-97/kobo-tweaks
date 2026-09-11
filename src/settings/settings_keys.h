72dc97b5fc15a1110cc03688dee3bf7fc5aaee47
Christian Gutierrez Areces <areces22@outlook.es>
2026-09-11 17:48:15 +0200

    Centralize Kobo Tweaks settings keys

---
+++ src/settings/settings_keys.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace SettingsKeys {
+    constexpr const char* ReadingBookmarkImage = "Reading/BookmarkImage";
+    constexpr const char* ReadingHeaderFooterHeightScale = "Reading/HeaderFooterHeightScale";
+    constexpr const char* ReadingHeaderFooterMargins = "Reading/HeaderFooterMargins";
+    constexpr const char* ReadingHeaderSpacerHeight = "Reading/HeaderSpacerHeight";
+    constexpr const char* ReadingFooterSpacerHeight = "Reading/FooterSpacerHeight";
+
+    constexpr const char* ReadingWidgetHeaderLeft = "Reading.Widget/HeaderLeft";
+    constexpr const char* ReadingWidgetHeaderCenter = "Reading.Widget/HeaderCenter";
+    constexpr const char* ReadingWidgetHeaderRight = "Reading.Widget/HeaderRight";
+    constexpr const char* ReadingWidgetFooterLeft = "Reading.Widget/FooterLeft";
+    constexpr const char* ReadingWidgetFooterCenter = "Reading.Widget/FooterCenter";
+    constexpr const char* ReadingWidgetFooterRight = "Reading.Widget/FooterRight";
+
+    constexpr const char* ReadingWidgetSeparator = "Reading.Widget/Separator";
+    constexpr const char* ReadingWidgetSpacing = "Reading.Widget/Spacing";
+
+    constexpr const char* ReadingWidgetBatteryStyle = "Reading.Widget.Battery/Style";
+    constexpr const char* ReadingWidgetBatteryStyleCharging = "Reading.Widget.Battery/StyleCharging";
+    constexpr const char* ReadingWidgetBatteryShowWhenBelow = "Reading.Widget.Battery/ShowWhenBelow";
+
+    constexpr const char* ReadingWidgetClock24hFormat = "Reading.Widget.Clock/24hFormat";
+}
